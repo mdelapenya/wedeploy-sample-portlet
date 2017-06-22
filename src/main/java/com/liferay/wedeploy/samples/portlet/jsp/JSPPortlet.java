b674bf7d183285c38e8c6f0180d6cede164415fb
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-06-22 07:44:13 +0200

    Use a category for wedeplot portlets

--- src/main/java/com/liferay/wedeploy/samples/portlet/jsp/JSPPortlet.java
+++ src/main/java/com/liferay/wedeploy/samples/portlet/jsp/JSPPortlet.java
@@ -27,7 +27,7 @@ import org.osgi.service.component.annotations.Component;
 	immediate = true,
 	property = {
 		"com.liferay.portlet.css-class-wrapper=portlet-jsp",
-		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.display-category=wedeploy.display-category",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.instanceable=true",
 		"javax.portlet.display-name=WeDeploy Portlet",
